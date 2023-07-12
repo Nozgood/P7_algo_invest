e1be0aed76230ce3f096110a57bbd5d0351a90c8
nowfeel <nowfeel@bliink.io>
2023-07-12 07:14:55 +0200

    doc: docstring

--- bruteforce.py
+++ bruteforce.py
@@ -7,6 +7,13 @@ STOCK_PRICE_INDEX = 1
 STOCK_INTERESTS_INDEX = 2
 
 def bruteforce(path):
+    """the main function of this package : extract csv rows, transform it into dictionaries and apply the 
+    bruteforce algorithm on it
+    
+
+    Args:
+        path (string): the path of the CSV file we want to treat
+    """
     rows = from_csv_to_rows(path)
     rows.pop(0)
     stocks = []
@@ -21,6 +28,14 @@ def bruteforce(path):
     print(f"the winner is : {winner} and the algorithm tooks {str(time_spend)} seconds to be done")
 
 def from_csv_to_rows(path: str):
+    """Receive the path of a csv file we want to extract, returns raw rows from this file
+
+    Args:
+        path (str): the path of the file we want to treat
+
+    Returns:
+        list: a list of rows (which is a list of list)
+    """
     rows = []
     with open(path, "r") as file:
         csv_reader = csv.reader(file)
@@ -29,12 +44,31 @@ def from_csv_to_rows(path: str):
     return rows
 
 def from_row_to_stock(row: list):
+    """Receive a list of rows and transform it into a dictionary to use it with keys
+
+    Args:
+        row (list): the list of rows we want to transform
+
+    Returns:
+        dict: a dictionary with the keys we chose and the values associate to it (key corresponds to the header of each colum of the csv file)
+    """
     raw_interest = int(row[STOCK_INTERESTS_INDEX].split("%")[0])
     interest = raw_interest / 100
     benefits = float(row[STOCK_PRICE_INDEX]) * interest
     return {"name": row[STOCK_NAME_INDEX], "price": row[STOCK_PRICE_INDEX], "interest": interest, "benefits": benefits}
 
 def recursive(stocks, combo=None):
+    """Received a list of dictionary and generate all possible combination for each value of each dictionary
+    It doesn't allow to use the same dictionary 2 times
+    It's a recursive function (it calls itself)
+
+    Args:
+        stocks (list): list of dictionaries we want to combine
+        combo (dict, optional): the last combination which has been generated. Defaults to None.
+
+    Returns:
+        list: a list of dictionary with all the combination 
+    """
     if combo is None:
         combo = {"name": "", "price": 0, "benefits": 0}
     combination = []
