b988e487ef01bd698773118cbf8262c4fe067363
nowfeel <nowfeel@bliink.io>
2023-07-17 19:29:16 +0200

    feat: optimized algorithm for compute best combination of stocks

--- optimized.py
+++ optimized.py
@@ -5,33 +5,25 @@ CSV_PATH = "./assets/dataset1_Python_P7.csv"
 STOCK_NAME_INDEX = 0
 STOCK_PRICE_INDEX = 1
 STOCK_INTERESTS_INDEX = 2
+INVESTMENT_BUDGET = 500
 
-def optimized(path: str):
+def main(path: str):
+    """main receive a path (as string) to a csv file to convert it into a list of dictionaries
+    Execute a function to find the best combination of dictionary, print it and the time maked to find it 
+
+    Args:
+        path (str): _description_
+    """
     rows = from_csv_to_rows(path)
     rows.pop(0)
-    stocks = []
-    for row in rows:
-        stocks.append(from_row_to_stock(row))
-    for stock in stocks:
-        if stock["price"] < 0:
-            print(stock)
-            print("OUUUUHOOO \n\n")
+    stocks = [from_row_to_stock(row) for row in rows]
     start_optimized = time.time()
-    solution = knapsack(500, stocks)
+    chosen_stocks = compute_best_combination(INVESTMENT_BUDGET, stocks)
     end_optimized = time.time()
-    price = 0
-    benefits = 0
-    all_names = ""
-    for action in solution:
-        price += action["price"]
-        benefits += action["benefits"]
-        all_names += action["name"] + ", "
-    print(solution)
-    print(price)
-    print(benefits)
-    print(all_names)
+    best_combination = process_best_combination(chosen_stocks)
+    print(f"The choosen stocks are {best_combination['name']} \nFor a price of {best_combination['price']} € \nAnd a benefice of {best_combination['benefits']} € on 2 years")
     time_spend = round(end_optimized - start_optimized, 2)
-    print(f"he algorithm tooks {str(time_spend)} seconds to be done")
+    print(f"The algorithm tooks {str(time_spend)} seconds to be done")
 
 def from_csv_to_rows(path: str):
     """Receive the path of a csv file we want to extract, returns raw rows from this file
@@ -58,47 +50,59 @@ def from_row_to_stock(row: list):
     Returns:
         dict: a dictionary with the keys we chose and the values associate to it (key corresponds to the header of each colum of the csv file)
     """
-    # raw_interest = int(row[STOCK_INTERESTS_INDEX].split("%")[0])
-    # interest = raw_interest / 100
-    # benefits = float(row[STOCK_PRICE_INDEX]) * interest
     price = int(float(row[STOCK_PRICE_INDEX]))
     if price < 0:
         return {"name":row[STOCK_NAME_INDEX], "price": 0, "benefits": 0 }
     return {"name": row[STOCK_NAME_INDEX], "price": (int(float(row[STOCK_PRICE_INDEX]))), "benefits": float(row[STOCK_INTERESTS_INDEX])}
 
-def knapsack(budget, stocks):
-    num_stocks = len(stocks)
-    value_matrix = [[0 for _ in range(budget + 1)] for _ in range(num_stocks + 1)]
-    choice_matrix = [[0 for _ in range(budget + 1)] for _ in range(num_stocks + 1)]
+def compute_best_combination(budget, stocks):
+    number_of_stocks = len(stocks)
+    stocks_combination_matrix = [[0 for _ in range(budget + 1)] for _ in range(number_of_stocks + 1)] # matrice of combination for stock/budget 
+    choiced_stocks_matrix = [[0 for _ in range(budget + 1)] for _ in range(number_of_stocks + 1)] # matrice who store which stock 
 
     # Build value_matrix[][]
-    for i in range(1, num_stocks + 1):
-        for w in range(budget + 1):
-            if stocks[i - 1]["price"] <= w:
-                # If the stock can be bought
-                without_i = value_matrix[i - 1][w]
-                remaining_w = w - stocks[i - 1]["price"]
-                if remaining_w >= 0:
-                    test = i - 1
-                    with_i = stocks[i - 1]["benefits"] + value_matrix[test][remaining_w]
-                    value_matrix[i][w] = max(without_i, with_i)
-                    # Store the choice made
-                    if with_i > without_i:
-                        choice_matrix[i][w] = 1
+    # For each stock i will iterate other all the possible values of the budget (from 1 to 500)
+    for stock_index in range(1, number_of_stocks + 1):
+        stock_to_compute_index = stock_index - 1
+        for budget_value in range(budget + 1):
+            if stocks[stock_to_compute_index]["price"] <= budget_value: # if the stock can be bought
+                combination_without_new_stock = stocks_combination_matrix[stock_to_compute_index][budget_value]
+                remaining_budget = budget_value - stocks[stock_to_compute_index]["price"]
+                if remaining_budget >= 0:
+                    combination_with_new_stock = stocks[stock_to_compute_index]["benefits"] + stocks_combination_matrix[stock_to_compute_index][remaining_budget]
+                    stocks_combination_matrix[stock_index][budget_value] = max(combination_without_new_stock, combination_with_new_stock)
+                    if combination_with_new_stock > combination_without_new_stock:
+                        choiced_stocks_matrix[stock_index][budget_value] = 1
                 else:
-                    value_matrix[i][w] = without_i
+                    stocks_combination_matrix[stock_index][budget_value] = combination_without_new_stock
             else:
-                # If the stock cannot be bought
-                value_matrix[i][w] = value_matrix[i - 1][w]
+                stocks_combination_matrix[stock_index][budget_value] = stocks_combination_matrix[stock_index - 1][budget_value]
 
-    # Retrieve the chosen stocks
     chosen_stocks = []
-    w = budget
-    for i in range(num_stocks, 0, -1):
-        if choice_matrix[i][w]:
-            chosen_stocks.append(stocks[i - 1]) 
-            w -= stocks[i - 1]["price"]
+    budget_value = budget
+    for stock_index in range(number_of_stocks, 0, -1):
+        if choiced_stocks_matrix[stock_index][budget_value]:
+            chosen_stocks.append(stocks[stock_index - 1]) 
+            budget_value -= stocks[stock_index - 1]["price"]
 
     return chosen_stocks
 
-optimized(CSV_PATH)
+
+def process_best_combination(stocks_combination: list):
+    """process_best_combination receive a list of stocks and returns a dictionary based on the sum of all the stocks in the received list
+
+    Args:
+        stocks_combination (list): list of stocks
+
+    Returns:
+        dict: the dictionary which represents the sum of all the stocks in the received list
+    """
+    final_combination = {"name": "", "price": 0, "benefits": 0}
+    for chosen_stock in stocks_combination:
+        final_combination["name"] += chosen_stock["name"] + ";"
+        final_combination["price"] += chosen_stock["price"]
+        final_combination["benefits"] += chosen_stock["benefits"]
+    return final_combination
+
+
+main(CSV_PATH)
